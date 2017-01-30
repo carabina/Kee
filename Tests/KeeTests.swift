a4cc53a159a3f494d67e0644f998a4df39884fff
Max Sokolov <i@maxsokolov.net>
2017-01-30 21:40:32 +0300

    xcode project setup

---
+++ Tests/KeeTests.swift
@@ -0,0 +1,36 @@
+//
+//  KeeTests.swift
+//  KeeTests
+//
+//  Created by Max Sokolov on 30/01/2017.
+//  Copyright © 2017 Kee. All rights reserved.
+//
+
+import XCTest
+@testable import Kee
+
+class KeeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
